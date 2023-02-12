a0ddf9929e66fe1d4e56f041a289ccb41a8e4a03
Gwynwhyvaar <tunde.ayoko@hotmail.com>
2023-02-12 23:08:44 +0100

    - added interfaces for comics, blogs and users

---
+++ Guenwhyvar.BookOfShadows/Guenwhyvar.BookOfShadows.NoSql/Abstract/IBlogEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using Guenwhyvar.BookOfShadows.NoSql.Entities;
+
+namespace Guenwhyvar.BookOfShadows.NoSql.Abstract
+{
+    public interface IBlogEntityRepository
+    {
+        Task<BlogEntity> GetBlogEntityAsync(string blogId);
+        Task<BlogEntity> GetBlogEntityBySelfUrlAsync(string selfUlr);
+        Task<List<BlogEntity>> GetAllBlogEntitiesAsync(string tag);
+        Task<List<BlogEntity>> GetAllBlogEntitiesAsync(string tag, string userName);
+        Task<string> AddBlogEntity(BlogEntity blogEntity);
+        Task DeleteBlogEntityAsync(string blogId);
+        Task UpdateBlogEntityAsync(string blogId, BlogEntity blogEntity);
+        Task<List<BlogEntity>> GetAllBlogEntitiesByUserNameAsync(string userName);
+    }
+}
